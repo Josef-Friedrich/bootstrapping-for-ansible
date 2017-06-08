83c765e53f09f15702a8f9b21cd1f2dd3d933a5f
Josef Friedrich <josef@friedrich.rocks>
2017-06-08 22:28:43 +0200

    Add code for ubuntu

--- bootstrapping-for-ansible.sh
+++ bootstrapping-for-ansible.sh
@@ -4,17 +4,21 @@
 
 # Run with: wget https://git.io/vHXIB -O - | sh
 
-su
+DISTRO=$(lsb_release -si)
+SERVICE=sshd
 
-passwd
+if [ "$DISTRO" = 'Ubuntu' ]; then
+  apt install openssh-server
+  SERVICE=ssh
+fi
 
-cat << EOF > /etc/ssh/sshd_conf
+cat << EOF >> /etc/ssh/sshd_conf
 PermitRootLogin yes
 PubkeyAuthentication yes
 PasswordAuthentication yes
 EOF
 
-systemctl start sshd.service
-systemctl enable sshd.service
+systemctl start "$SERVICE".service
+systemctl enable "$SERVICE".service
 
 echo lol
