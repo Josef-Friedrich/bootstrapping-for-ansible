3b0d7ef56150b57cd3b71ff3fffd2eb80a9337a6
Josef Friedrich <josef@friedrich.rocks>
2017-06-08 18:58:33 +0000

    ssh config

--- bootstrapping-for-ansible.sh
+++ bootstrapping-for-ansible.sh
@@ -4,4 +4,17 @@
 
 # Run with: wget https://git.io/vHXIB -O - | sh
 
+su
+
+passwd
+
+cat << EOF > /etc/ssh/sshd_conf
+PermitRootLogin yes
+PubkeyAuthentication yes
+PasswordAuthentication yes
+EOF
+
+systemctl start sshd.service
+systemctl enable sshd.service
+
 echo lol
